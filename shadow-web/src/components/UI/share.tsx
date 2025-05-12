bc13c1bb3fd8cf5dcf44959f65c1b9336d64f6df
MadsKelberg <35922365+MadsKelberg@users.noreply.github.com>
2025-05-12 17:35:19 +0200

    fix: Share modal

--- shadow-web/src/components/UI/share.tsx
+++ shadow-web/src/components/UI/share.tsx
@@ -34,9 +34,6 @@ const ShareModal = (props: ShareModalProps) => {
   return (
     <div className="fixed inset-0 z-10 overflow-y-auto">
       <div className="flex min-h-screen items-center justify-center px-4 pt-4 text-center sm:block sm:p-0">
-        <div className="fixed inset-0 transition-opacity">
-          <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
-        </div>
         <span className="hidden sm:inline-block sm:h-screen sm:align-middle"></span>
         <div className="inline-block transform rounded-lg bg-gray-700 px-4 pt-5 pb-4 text-left align-bottom shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6 sm:align-middle">
           <div className="align-center flex w-full flex-col sm:items-start">
