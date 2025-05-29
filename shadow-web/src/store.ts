2a73eae20018fd9b4ccb02e374f86cd51ae0091f
Oliver Geneser <hello@geneser.xyz>
2025-05-29 17:11:48 +0200

    refactor: cleanup of store

--- shadow-web/src/store.ts
+++ shadow-web/src/store.ts
@@ -8,12 +8,14 @@ import {
   SignalOfferData,
 } from "shadow-shared";
 import { UUID } from "crypto";
+import { deriveSecretKey, encryptMessage } from "./utils/encryption";
 import {
-  decryptMessage,
-  deriveSecretKey,
-  encryptMessage,
-} from "./utils/encryption";
-import { FIFOQueue } from "./utils/queue";
+  chatMessageQueue,
+  CustomFile,
+  messageQueue,
+  receiveBuffers,
+  receiveSizes,
+} from "./utils/queue";
 
 export interface rtcConnectionsArray {
   clientId: string;
@@ -28,7 +30,9 @@ export type ChatMessages = {
 interface WebRTCConnections {
   [key: string]: RTCPeerConnection;
 }
+
 type DataChannelConnections = Record<string, RTCDataChannel>;
+
 export enum ReadyState {
   UNINSTANTIATED = -1,
   CONNECTING = 0,
@@ -36,6 +40,7 @@ export enum ReadyState {
   CLOSING = 2,
   CLOSED = 3,
 }
+
 export const webRTCConfig: RTCConfiguration = {
   iceServers: [
     {
@@ -46,277 +51,10 @@ export const webRTCConfig: RTCConfiguration = {
   ],
 };
 
-const receiveBuffers: { [id: string]: ArrayBuffer[] } = {};
-const receiveSizes: { [id: string]: number } = {};
-
 export interface Message {
   id: string;
   data: ArrayBuffer;
 }
-
-const messageProcessing = async (message: Message): Promise<void> => {
-  if (messageQueue.isLocked(message.id)) return;
-  const keyPair = store.select((state) => state.keyPair).get();
-
-  const clients = store.select((state) => state.clients).get();
-  const client = clients.find((client) => client.clientId === message.id);
-
-  const files = store.select((state) => state.files).get();
-  const file = files[message.id];
-  let receiveFile: CustomFile;
-  if (file) {
-    if (file.data === undefined && file.status) {
-      receiveFile = {
-        data: {
-          publicKey: client!.publicKey,
-          name: "",
-          size: 0,
-          type: "",
-        },
-        status: files[message.id].status,
-      };
-    } else {
-      receiveFile = files[message.id];
-    }
-  } else {
-    receiveFile = {
-      data: {
-        publicKey: client!.publicKey,
-        name: "",
-        size: 0,
-        type: "",
-      },
-      status: {
-        activity: "pending",
-        progress: 0,
-      },
-    };
-  }
-
-  const receiveBuffer = receiveBuffers[message.id] ?? [];
-
-  let receivedSize = receiveSizes[message.id] ?? 0;
-
-  const messageArray = new Uint8Array(message.data);
-
-  const packetType = messageArray[0];
-
-  if (packetType === 0b00000001) {
-    try {
-      if (receiveFile.data) {
-        const publicKey = await window.crypto.subtle.importKey(
-          "jwk",
-          receiveFile.data.publicKey,
-          {
-            name: "ECDH",
-            namedCurve: "P-384",
-          },
-          true,
-          [],
-        );
-
-        const secretKey = await deriveSecretKey(keyPair!.privateKey, publicKey);
-
-        const iv = messageArray.slice(1, 9);
-
-        const chunkData = messageArray.slice(1 + iv.length);
-
-        const data = await decryptMessage(secretKey, iv, chunkData);
-
-        const decodedData = new TextDecoder("utf-8").decode(data);
-        const parsedMetadata = JSON.parse(decodedData);
-
-        store.trigger.setFile({
-          peerId: message.id,
-          status: { activity: "receiving" },
-          file: { ...parsedMetadata, publicKey: receiveFile.data.publicKey },
-        });
-        store.trigger.setSenderAwaitingApproval({
-          peerId: message.id,
-          fileId: parsedMetadata.id,
-          fileName: parsedMetadata.name,
-        });
-        return;
-      }
-    } catch (e) {
-      console.log(e);
-      return;
-    }
-  } else if (packetType === 0b00000000) {
-    const sendersAwaitingApproval = store
-      .select((state) => state.sendersAwaitingApproval)
-      .get();
-    if (!sendersAwaitingApproval.some((s) => s.peerId === message.id)) {
-      if (receiveFile !== undefined && receiveFile.data) {
-        const publicKey = await window.crypto.subtle.importKey(
-          "jwk",
-          receiveFile.data.publicKey,
-          {
-            name: "ECDH",
-            namedCurve: "P-384",
-          },
-          true,
-          [],
-        );
-
-        const secretKey = await deriveSecretKey(keyPair!.privateKey, publicKey);
-
-        const iv = messageArray.slice(1, 9);
-
-        const chunkData = messageArray.slice(1 + iv.length);
-
-        const data = await decryptMessage(secretKey, iv, chunkData);
-
-        if (data) {
-          receiveBuffer.push(data);
-          receivedSize += data.byteLength;
-          const newProgress = Math.floor(
-            (receivedSize / receiveFile.data["size"]) * 100,
-          );
-          if (
-            newProgress - (receiveFile.status?.progress ?? 0) > 1 ||
-            (receiveFile.status?.progress ?? 0 === 0)
-          ) {
-            store.trigger.setFileStatus({
-              peerId: message.id,
-              status: { activity: "receiving", progress: newProgress },
-            });
-          }
-
-          if (receivedSize == receiveFile.data["size"]) {
-            const blob = new Blob(receiveBuffer, {
-              type: receiveFile.data["type"],
-            });
-            const fileURL = URL.createObjectURL(blob);
-            const a = document.createElement("a");
-            a.href = fileURL;
-            a.download = receiveFile.data.name || "download";
-            document.body.appendChild(a);
-            a.click();
-            document.body.removeChild(a);
-            URL.revokeObjectURL(fileURL);
-            receiveBuffers[message.id] = [];
-            receiveSizes[message.id] = 0;
-
-            store.trigger.removeFile({
-              peerId: message.id,
-            });
-
-            return;
-          }
-          receiveBuffers[message.id] = receiveBuffer;
-          receiveSizes[message.id] = receivedSize;
-        }
-      }
-    }
-  } else if (packetType === 0b00000010) {
-    if (receiveFile.data) {
-      const publicKey = await window.crypto.subtle.importKey(
-        "jwk",
-        receiveFile.data.publicKey,
-        {
-          name: "ECDH",
-          namedCurve: "P-384",
-        },
-        true,
-        [],
-      );
-
-      const secretKey = await deriveSecretKey(keyPair!.privateKey, publicKey);
-
-      const iv = messageArray.slice(1, 9);
-
-      const chunkData = messageArray.slice(1 + iv.length);
-
-      const data = await decryptMessage(secretKey, iv, chunkData);
-
-      if (data) {
-        const decodedData = new TextDecoder("utf-8").decode(data);
-        const parsedMetadata = JSON.parse(decodedData);
-
-        if (parsedMetadata.accepted === true) {
-          store.trigger.removeAwaitingApproval({
-            fileId: parsedMetadata.fileId,
-          });
-        } else {
-          const fileChannels = store
-            .select((state) => state.fileChannelConnections)
-            .get();
-          fileChannels[message.id].close();
-          store.trigger.removeAwaitingApproval({
-            fileId: parsedMetadata.fileId,
-          });
-        }
-      }
-    }
-  } else {
-    console.log("Unknown packetType:", packetType);
-  }
-};
-
-const chatMessageProcessing = async (message: Message): Promise<void> => {
-  const keyPair = store.select((state) => state.keyPair).get();
-  const clients = store.select((state) => state.clients).get();
-  const client = clients.find((client) => client.clientId === message.id);
-
-  if (!client) throw new Error("Missing client");
-
-  const messageArray = new Uint8Array(message.data);
-
-  const packetType = messageArray[0];
-
-  if (packetType === 0b00000000) {
-    try {
-      const publicKey = await window.crypto.subtle.importKey(
-        "jwk",
-        client.publicKey,
-        {
-          name: "ECDH",
-          namedCurve: "P-384",
-        },
-        true,
-        [],
-      );
-
-      const secretKey = await deriveSecretKey(keyPair!.privateKey, publicKey);
-
-      const iv = messageArray.slice(1, 9);
-
-      const chunkData = messageArray.slice(1 + iv.length);
-
-      const data = await decryptMessage(secretKey, iv, chunkData);
-
-      const decodedData = new TextDecoder("utf-8").decode(data);
-
-      store.trigger.setNewChatMessage({
-        message: decodedData,
-        peerId: message.id,
-      });
-      return;
-    } catch (e) {
-      console.log(e);
-      return;
-    }
-  }
-};
-
-type CustomFile = {
-  data:
-    | {
-        name: string;
-        size: number;
-        type: string;
-        publicKey: JsonWebKey;
-      }
-    | undefined;
-  status: {
-    activity: "sending" | "receiving" | "pending";
-    progress: number;
-  };
-};
-
-const messageQueue = new FIFOQueue(messageProcessing);
-const chatMessageQueue = new FIFOQueue(chatMessageProcessing);
 
 type SendersApproval = {
   peerId: string;
--- shadow-web/src/utils/queue.ts
+++ shadow-web/src/utils/queue.ts
@@ -1,4 +1,5 @@
-import { Message } from "../store";
+import { Message, store } from "../store";
+import { decryptMessage, deriveSecretKey } from "./encryption";
 
 export class FIFOQueue {
   private queue: Message[] = [];
@@ -50,3 +51,270 @@ export class FIFOQueue {
     this.isProcessing = false;
   }
 }
+
+export type CustomFile = {
+  data:
+    | {
+        name: string;
+        size: number;
+        type: string;
+        publicKey: JsonWebKey;
+      }
+    | undefined;
+  status: {
+    activity: "sending" | "receiving" | "pending";
+    progress: number;
+  };
+};
+
+export const receiveBuffers: { [id: string]: ArrayBuffer[] } = {};
+export const receiveSizes: { [id: string]: number } = {};
+
+const messageProcessing = async (message: Message): Promise<void> => {
+  if (messageQueue.isLocked(message.id)) return;
+  const keyPair = store.select((state) => state.keyPair).get();
+
+  const clients = store.select((state) => state.clients).get();
+  const client = clients.find((client) => client.clientId === message.id);
+
+  const files = store.select((state) => state.files).get();
+  const file = files[message.id];
+  let receiveFile: CustomFile;
+  if (file) {
+    if (file.data === undefined && file.status) {
+      receiveFile = {
+        data: {
+          publicKey: client!.publicKey,
+          name: "",
+          size: 0,
+          type: "",
+        },
+        status: files[message.id].status,
+      };
+    } else {
+      receiveFile = files[message.id];
+    }
+  } else {
+    receiveFile = {
+      data: {
+        publicKey: client!.publicKey,
+        name: "",
+        size: 0,
+        type: "",
+      },
+      status: {
+        activity: "pending",
+        progress: 0,
+      },
+    };
+  }
+
+  const receiveBuffer = receiveBuffers[message.id] ?? [];
+
+  let receivedSize = receiveSizes[message.id] ?? 0;
+
+  const messageArray = new Uint8Array(message.data);
+
+  const packetType = messageArray[0];
+
+  if (packetType === 0b00000001) {
+    try {
+      if (receiveFile.data) {
+        const publicKey = await window.crypto.subtle.importKey(
+          "jwk",
+          receiveFile.data.publicKey,
+          {
+            name: "ECDH",
+            namedCurve: "P-384",
+          },
+          true,
+          [],
+        );
+
+        const secretKey = await deriveSecretKey(keyPair!.privateKey, publicKey);
+
+        const iv = messageArray.slice(1, 9);
+
+        const chunkData = messageArray.slice(1 + iv.length);
+
+        const data = await decryptMessage(secretKey, iv, chunkData);
+
+        const decodedData = new TextDecoder("utf-8").decode(data);
+        const parsedMetadata = JSON.parse(decodedData);
+
+        store.trigger.setFile({
+          peerId: message.id,
+          status: { activity: "receiving" },
+          file: { ...parsedMetadata, publicKey: receiveFile.data.publicKey },
+        });
+        store.trigger.setSenderAwaitingApproval({
+          peerId: message.id,
+          fileId: parsedMetadata.id,
+          fileName: parsedMetadata.name,
+        });
+        return;
+      }
+    } catch (e) {
+      console.log(e);
+      return;
+    }
+  } else if (packetType === 0b00000000) {
+    const sendersAwaitingApproval = store
+      .select((state) => state.sendersAwaitingApproval)
+      .get();
+    if (!sendersAwaitingApproval.some((s) => s.peerId === message.id)) {
+      if (receiveFile !== undefined && receiveFile.data) {
+        const publicKey = await window.crypto.subtle.importKey(
+          "jwk",
+          receiveFile.data.publicKey,
+          {
+            name: "ECDH",
+            namedCurve: "P-384",
+          },
+          true,
+          [],
+        );
+
+        const secretKey = await deriveSecretKey(keyPair!.privateKey, publicKey);
+
+        const iv = messageArray.slice(1, 9);
+
+        const chunkData = messageArray.slice(1 + iv.length);
+
+        const data = await decryptMessage(secretKey, iv, chunkData);
+
+        if (data) {
+          receiveBuffer.push(data);
+          receivedSize += data.byteLength;
+          const newProgress = Math.floor(
+            (receivedSize / receiveFile.data["size"]) * 100,
+          );
+          if (
+            newProgress - (receiveFile.status?.progress ?? 0) > 1 ||
+            (receiveFile.status?.progress ?? 0 === 0)
+          ) {
+            store.trigger.setFileStatus({
+              peerId: message.id,
+              status: { activity: "receiving", progress: newProgress },
+            });
+          }
+
+          if (receivedSize == receiveFile.data["size"]) {
+            const blob = new Blob(receiveBuffer, {
+              type: receiveFile.data["type"],
+            });
+            const fileURL = URL.createObjectURL(blob);
+            const a = document.createElement("a");
+            a.href = fileURL;
+            a.download = receiveFile.data.name || "download";
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+            URL.revokeObjectURL(fileURL);
+            receiveBuffers[message.id] = [];
+            receiveSizes[message.id] = 0;
+
+            store.trigger.removeFile({
+              peerId: message.id,
+            });
+
+            return;
+          }
+          receiveBuffers[message.id] = receiveBuffer;
+          receiveSizes[message.id] = receivedSize;
+        }
+      }
+    }
+  } else if (packetType === 0b00000010) {
+    if (receiveFile.data) {
+      const publicKey = await window.crypto.subtle.importKey(
+        "jwk",
+        receiveFile.data.publicKey,
+        {
+          name: "ECDH",
+          namedCurve: "P-384",
+        },
+        true,
+        [],
+      );
+
+      const secretKey = await deriveSecretKey(keyPair!.privateKey, publicKey);
+
+      const iv = messageArray.slice(1, 9);
+
+      const chunkData = messageArray.slice(1 + iv.length);
+
+      const data = await decryptMessage(secretKey, iv, chunkData);
+
+      if (data) {
+        const decodedData = new TextDecoder("utf-8").decode(data);
+        const parsedMetadata = JSON.parse(decodedData);
+
+        if (parsedMetadata.accepted === true) {
+          store.trigger.removeAwaitingApproval({
+            fileId: parsedMetadata.fileId,
+          });
+        } else {
+          const fileChannels = store
+            .select((state) => state.fileChannelConnections)
+            .get();
+          fileChannels[message.id].close();
+          store.trigger.removeAwaitingApproval({
+            fileId: parsedMetadata.fileId,
+          });
+        }
+      }
+    }
+  } else {
+    console.log("Unknown packetType:", packetType);
+  }
+};
+
+const chatMessageProcessing = async (message: Message): Promise<void> => {
+  const keyPair = store.select((state) => state.keyPair).get();
+  const clients = store.select((state) => state.clients).get();
+  const client = clients.find((client) => client.clientId === message.id);
+
+  if (!client) throw new Error("Missing client");
+
+  const messageArray = new Uint8Array(message.data);
+
+  const packetType = messageArray[0];
+
+  if (packetType === 0b00000000) {
+    try {
+      const publicKey = await window.crypto.subtle.importKey(
+        "jwk",
+        client.publicKey,
+        {
+          name: "ECDH",
+          namedCurve: "P-384",
+        },
+        true,
+        [],
+      );
+
+      const secretKey = await deriveSecretKey(keyPair!.privateKey, publicKey);
+
+      const iv = messageArray.slice(1, 9);
+
+      const chunkData = messageArray.slice(1 + iv.length);
+
+      const data = await decryptMessage(secretKey, iv, chunkData);
+
+      const decodedData = new TextDecoder("utf-8").decode(data);
+
+      store.trigger.setNewChatMessage({
+        message: decodedData,
+        peerId: message.id,
+      });
+      return;
+    } catch (e) {
+      console.log(e);
+      return;
+    }
+  }
+};
+
+export const messageQueue = new FIFOQueue(messageProcessing);
+export const chatMessageQueue = new FIFOQueue(chatMessageProcessing);
